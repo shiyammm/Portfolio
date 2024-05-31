f0ed65290790bb867be50756846577f998a7de9d
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-05-31 22:57:35 +0530

    Customize tailwind and global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,8 @@ const config: Config = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './containers/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -12,6 +13,22 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'black-1': '#1d1d1f',
+        'black-2': '#121315',
+        cyan: '#37EBA9',
+        violet: '#7A8FFF',
+      },
+      fontFamily: {
+        acme: ['var(--font-acme)'],
+        'circular-book': ['var(--font-circular-book)'],
+        'circular-medium': ['var(--font-circular-medium)'],
+        'neue-montreal': ['var(--font-neue-montreal)'],
+        'roslindale-display': ['var(--font-roslindale-display)'],
+        'gt-alpina': ['var(--font-gt-alpina)'],
+        'bebasNeue-regular': ['var(--font-bebasNeue-regular)'],
+        'canopee-regular': ['var(--font-canopee-Regular)'],
       },
     },
   },
